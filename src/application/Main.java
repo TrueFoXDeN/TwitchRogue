b8ea0ce7340b844405eb1f1bc6e214f7788b801c
FoX <laurenz.dilba@hotmail.de>
2019-08-23 01:18:07 +0200

    Added Window

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
 
+import drawing.Display;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
-
+        Display display = new Display(1280, 720, "Twitch Roguelike by FoX and KoKoKotlin");
         GameLoop loop = new GameLoop();
         loop.start();
     }
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,4 +1,5 @@
 package game;
 
 public class Player {
+    int x, y;
 }
