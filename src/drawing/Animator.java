136e79432fe6d0d43014a84b53e47a8f5b4c7353
KoKoKotlin <labruzzler@gmail.com>
2019-08-26 22:08:07 +0200

    added animation class

---
+++ src/drawing/Animator.java
@@ -0,0 +1,36 @@
+package drawing;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animator {
+
+    public Animator(double switchTime, List<BufferedImage> sprites, Rule rule) {
+        this.switchTime = switchTime;
+        this.sprites = sprites;
+        this.rule = rule;
+    }
+
+    private double currTime = 0;
+    private int state = 0;
+    private double switchTime;
+
+    private List<BufferedImage> sprites = new ArrayList<>();
+    private Rule rule;
+
+    public void update(double delta) {
+        currTime -= delta;
+        rule.fire(state);
+
+        if(currTime <= 0) {
+            currTime = switchTime;
+        }
+
+    }
+
+    @FunctionalInterface
+    public interface Rule {
+        void fire(int state);
+    }
+}
