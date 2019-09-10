095a8847f82fdf47249307992f2da667ae0d4834
KoKoKotlin <labruzzler@gmail.com>
2019-09-10 17:12:03 +0200

    Debugging vision

--- src/drawing/BattleBackground.java
+++ src/drawing/BattleBackground.java
@@ -14,8 +14,8 @@ public class BattleBackground implements Drawable {
     private final OpenSimplexNoise noise = new OpenSimplexNoise(System.nanoTime());
     private AtomicReference<Double> t = new AtomicReference<>(0.0);
 
-    private final double TIME_OFFSET = 0.01;
-    private final double SPACE_OFFSET = 0.0001;
+    private final double TIME_OFFSET = 0.001;
+    private final double SPACE_OFFSET = 0.01;
 
     public BattleBackground() {
         Timer timer = new Timer();
@@ -24,7 +24,7 @@ public class BattleBackground implements Drawable {
             public void run() {
                 t.set(t.get() + TIME_OFFSET);
             }
-        }, 0, 10);
+        }, 0, 1);
     }
 
 
@@ -33,14 +33,13 @@ public class BattleBackground implements Drawable {
         BufferedImage back = new BufferedImage(Main.display.w0, Main.display.h0, BufferedImage.TYPE_3BYTE_BGR);
         double dx = 0.0;
         double dy = 0.0;
-        for (int x = 0; x < back.getWidth(); x++, dx += 0.01) {
-            for (int y = 0; y < back.getHeight(); y++) {
-                int redValue = Math.abs((int) (255 * noise.eval(dx, dy, t.get())));
+        for (int x = 0; x < back.getWidth(); x++, dx += SPACE_OFFSET) {
+            dy = 0.0;
+            for (int y = 0; y < back.getHeight(); y++, dy += SPACE_OFFSET) {
+                int redValue = (int) (125 * (noise.eval(dx, dy, t.get()) + 1) / 2 );
                 Color pixelColor = new Color(redValue, 0, 0);
                 back.setRGB(x, y, pixelColor.getRGB());
-                dx += SPACE_OFFSET;
             }
-            dy += SPACE_OFFSET;
         }
 
         g.drawImage(back, 0, 0, null);
